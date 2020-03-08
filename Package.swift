7e878c59d94b9231ae9140d327b758822d21e8f5
sryxzh <55874724+sryxzh@users.noreply.github.com>
2020-03-09 00:11:05 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,17 +1,26 @@
-
-
 import PackageDescription
 
-let package = Package(name: "CocoaMQTT",
-                      platforms: [.iOS(.v10)],
-                      products: [.library(name: "CocoaMQTT",
-                                          targets: ["CocoaMQTT"])],
-                      targets: [.target(name: "CocoaMQTT",
-                                        path: "Source"),
-                                .testTarget(name: "CocoaMQTTTests",
-                                            dependencies: ["CocoaMQTT"],
-                                            path: "CocoaMQTTTests")],
-                      swiftLanguageVersions: [.v5])
+let package = Package(
+    name: "MQTT",
+    products: [
+        .library(name: "MQTT", targets: ["MQTT"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket.git", from: "7.6.3"),
+        .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.0.2"),
+    ],
+    targets: [
+        .target(
+            name: "MQTT",
+            dependencies: ["CocoaAsyncSocket", "Starscream"]),
+        .testTarget(
+            name: "CocoaMQTTTests",
+            dependencies: ["MQTT"]),
+    ]，
+  swiftLanguageVersions: [.v5])
+)
+
+                    
 
 
 
